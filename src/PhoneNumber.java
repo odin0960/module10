13e451ca0e2ec8aa2534b30f32482c6ab4b1151f
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-04-09 19:29:21 +0300

    module10

---
+++ src/PhoneNumber.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneNumber {
+
+    public static void readPhoneNumber() {
+        try (FileInputStream file = new FileInputStream("phone.txt")) {
+            byte[] buffer = new byte[file.available()];
+            file.read(buffer);
+
+            Pattern pattern = Pattern.compile("(\\(\\d{3}\\)\\s\\d{3}-\\d{4})|(\\d{3}-\\d{3}-\\d{4})");
+            Matcher matcher = pattern.matcher(new String(buffer));
+
+           while (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+class PhoneNumberTest {
+    public static void main(String[] args) {
+        PhoneNumber phoneNumber = new PhoneNumber();
+        phoneNumber.readPhoneNumber();
+    }
+}
