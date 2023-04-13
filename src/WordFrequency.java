748dd4321f0c18ebb0aa93ce1aa285882f0f1bb9
odin0960 <121524275+odin0960@users.noreply.github.com>
2023-04-13 11:14:35 +0300

    new comments

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -16,7 +16,7 @@ public class WordFrequency {
             }
             String[] words = str.toString().strip().split("\\s*(\\s)\\s*");
 
-            return words;
+            return words;                    // весь текст з файлу у вигляді масиву слів
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
@@ -25,14 +25,14 @@ public class WordFrequency {
 
     public void countWords(String[] words) {
         // рахує кількість кожного слова у тексті (масиві),
-        // формує мапу - "слово - кількость" та виводить у консоль
-        ArrayList<String> key = new ArrayList<>();
+        // формує мапу - "слово - кількість" та виводить у консоль
+        ArrayList<String> key = new ArrayList<>();   // колекція унікальних слів
         for (String word : words) {
             if (!key.contains(word))
                 key.add(word);
         }
 
-        ArrayList<Integer> value = new ArrayList<>();
+        ArrayList<Integer> value = new ArrayList<>();  // колекція кількостей слів у тексті (індекси співпадають з колекцією слів)
         for (int i = 0; i < key.size(); i++) {
             int count = 0;
             for (String word : words) {
@@ -42,19 +42,23 @@ public class WordFrequency {
             value.add(count);
         }
 
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();  // мапа "слово - кількість"
         for (int i = 0; i < key.size(); i++) {
             map.put(key.get(i), value.get(i));
         }
 
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
-        for (Map.Entry<String, Integer> entry : list) {
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());   // мапа у вигляді списка
+        list.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));         // сортування списка з мапи по значенню
+        for (Map.Entry<String, Integer> entry : list) {                            // та виведення у консоль кожної пари
             System.out.println(entry.getKey() + " " + entry.getValue());
-
+        }
+// інші робочі варіанти сортування та виведення у консоль:
+//
 //      map.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
 //                .forEach(System.out::println);
-        }
-    }
+
+//        map.entrySet().stream()
+//                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+//                .forEach(value -> System.out.println(value.getKey() + " " + value.getValue()));        }
 }
 
